05224f05d0b3e1d4cd283ff2850e2d77901031bb
jayc722 <jaewon8469@gmail.com>
2024-12-30 12:38:27 +0900

    자바 1일차 : Hello World 예제

---
+++ Java_lect1230/src/day001/Ex01_HelloWorld.java
@@ -0,0 +1,34 @@
+package day001;
+
+//한줄주석			//가 나온 뒤부터 주석
+
+/*
+ * 여러줄 주석. 이 사이에 있는 모든 글자들이 주석입니다.
+ */
+
+public class Ex01_HelloWorld {
+	
+	/**
+	 * 메소드를 설명할때 사용하는 주석입니다.
+	 * @param args
+	 */
+
+	//프로그램이 실행되려면 main이 있어야 한다.
+	public static void main(String[] args) {
+		// 들여쓰기는 Tab(보통 네칸)
+		// 내어쓰기는 Shift + Tab  
+
+		System.out.println("Hello World!\n");//Hello World!를 출력	
+		
+		
+	}
+	public static void main2(String[] args) {
+		// 들여쓰기는 Tab(보통 네칸)
+		// 내어쓰기는 Shift + Tab  
+
+		System.out.println("Hello World!\n");//Hello World!를 출력	
+		
+		
+	}
+
+}
