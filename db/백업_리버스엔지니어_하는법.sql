d82dc6f650a19e859c08dcb792d90487c7b8efb1
jayc722 <jaewon8469@gmail.com>
2025-03-17 10:35:09 +0900

    Create 백업_리버스엔지니어_하는법.sql

---
+++ db/백업_리버스엔지니어_하는법.sql
@@ -0,0 +1,6 @@
+# 수업 시간 DDL DML 자료 세팅 -> 실수로 빼먹을때? / 만들어진 데이터베이스 내보내서 그대로 세팅할 때  
+
+-- 좌측 네비게이터에서 ADMINISTRATION -> DATA EXPORT -> 한뒤 IMPORT하면 그대로 가져와짐
+
+
+-- DATABASE에 REVERSE ENGINEER 해서 NEXT 쭉 ->역으로 ERD 만들기 가능
