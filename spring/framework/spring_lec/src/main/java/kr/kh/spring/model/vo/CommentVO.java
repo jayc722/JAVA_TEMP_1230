ae1d812f6245cd4ea1365decea4adfd770474899
jayc722 <jaewon8469@gmail.com>
2025-03-27 11:51:33 +0900

    vo클래스도 생성

---
+++ spring/framework/spring_lec/src/main/java/kr/kh/spring/model/vo/CommentVO.java
@@ -0,0 +1,20 @@
+package kr.kh.spring.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CommentVO {
+
+	//귀찮으니 private 생략
+	int co_num;
+	String co_content;
+	Date co_date;
+	int co_ori_num;
+	String co_del;
+	String co_me_id;
+	int co_po_num;
+	
+	
+}
