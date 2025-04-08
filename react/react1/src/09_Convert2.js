5aaca82f99e9e6e39b316859f096029a3063a9cb
jayc722 <jaewon8469@gmail.com>
2025-04-08 15:31:38 +0900

    m->cm cm->m 전환 버튼

--- react/react1/src/09_Convert2.js
+++ react/react1/src/09_Convert2.js
@@ -7,16 +7,26 @@ cm->m 클릭하면 아래 입력창이 쓰기모드로 변환되도록
 */
 function Convert2(){
 
+	let [state, isState] = useState(false);
+
+	function mcm(){
+		if(state) isState(false);
+
+	}
+	function cmm(){
+		if(!state) isState(true);
+	}
+
 	return(
 		<div>
-			<button>m-&gt;cm</button>
-			<button>cm-&gt;m</button>
+			<button onClick={mcm}>m-&gt;cm</button>
+			<button onClick={cmm}>cm-&gt;m</button>
 
 			<div>
-				<input type="number" disabled={false}/>
+				<input type="number" disabled={state}/>
 				<button>변환</button>
 			</div>
-			<input type="number" disabled={true}/>
+			<input type="number" disabled={!state}/>
 		</div>
 	)
 
