75b9c172813fd8be012423f6f59bfafb4881fd50
jayc722 <jaewon8469@gmail.com>
2025-04-08 15:22:00 +0900

    강사님

--- react/react1/src/08_Convert.js
+++ react/react1/src/08_Convert.js
@@ -4,17 +4,20 @@ import { useState } from "react";
 /* 미터를 입력해서 변환을 클릭하면 센치미터로 변환되는 코드 작성 */
 function Convert(){
 
-	let [meter, setMeter] = useState("");
-	let [centiMeter, setCentiMeter] = useState("");
+	let [amount, setAmount] = useState(0);
+	let [res, setRes] = useState("");
 
+	function convert(){
+		setRes(amount / 100);
+	}
 
 	return(
 		<div>
 			<div>
-				<input type="text" onChange={(e)=>{setMeter(e.target.value)}}/>
-				<button onClick={(e)=>{setCentiMeter(meter / 100)}}>변환</button>
+				<input type="number" onChange={(e)=>{setAmount(e.target.value)}}/>
+				<button onClick={convert}>변환</button>
 			</div>
-			<input type="text" readOnly value={centiMeter}/>
+			<input type="number" readOnly value={res}/>
 		</div>
 	)
 
