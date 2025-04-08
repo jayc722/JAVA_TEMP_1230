68c6303932525cc5a9819b4461dcedd9250dcc16
jayc722 <jaewon8469@gmail.com>
2025-04-08 16:41:00 +0900

    강사님(내가한건 비동기라 바로바로 적용이 안됐네)

--- react/react1/src/10_Convert3.js
+++ react/react1/src/10_Convert3.js
@@ -7,13 +7,24 @@ function Convert3(){
 	let [amount1, setAmount1] = useState(0);
 	let [res1, setRes1] = useState(0);
 
+	function mToCm(e){
+		setAmount1(e.target.value);
+		setRes1(e.target.value * 100);
+	}
+
+	function cmToM(e){
+		setRes1(e.target.value);
+		setAmount1(e.target.value / 100);
+	}
+
+
 	return(
 		<div>
 			<div>
-				<input type="number" disabled={flag} onChange={(e)=>{setAmount1(e.target.value); setRes1(amount1 * 100);}} value={amount1}/>
+				<input type="number" disabled={flag} onChange={mToCm} value={amount1}/>
 				<button onClick={()=>isFlag(!flag)}>변환</button>
 			</div>
-			<input type="number" disabled={!flag} value={res1} onChange={(e)=>{setRes1(e.target.value); setAmount1(res1 / 100)}}/>
+			<input type="number" disabled={!flag} value={res1} onChange={cmToM}/>
 		</div>
 	)
 
