d9894183cd206e3cd1ee8f3fbc2e0285d5ea3151
jayc722 <jaewon8469@gmail.com>
2025-04-10 15:14:38 +0900

    회원가입 시 입력 칸 내용 제거

--- react/router2/src/Signup.js
+++ react/router2/src/Signup.js
@@ -88,19 +88,19 @@ function Signup(){
 			<form onSubmit={submit}>
 				<div className="mb-3 mt-3">
 					<label htmlFor="id" className="form-label">아이디:</label>
-					<input type="text" className="form-control" id="id" placeholder="아이디를 입력하세요." name="me_id" /*onChange={e=>setId(e.target.value)}*/onChange={change}/>
+					<input type="text" className="form-control" id="id" placeholder="아이디를 입력하세요." name="me_id" value={data.me_id} /*onChange={e=>setId(e.target.value)}*/onChange={change}/>
 				</div>
 				<div className="mb-3 mt-3">
 					<label htmlFor="pw" className="form-label">비번:</label>
-					<input type="password" className="form-control" id="pw" placeholder="비밀번호를 입력하세요." name="me_pw" /*onChange={e=>setPw(e.target.value)}*/onChange={change}/>
+					<input type="password" className="form-control" id="pw" placeholder="비밀번호를 입력하세요." name="me_pw" value={data.me_pw} /*onChange={e=>setPw(e.target.value)}*/onChange={change}/>
 				</div>
 				<div className="mb-3 mt-3">
 					<label htmlFor="pw2" className="form-label">비번확인:</label>
-					<input type="password" className="form-control" id="pw2" placeholder="비밀번호를 입력하세요." name="me_pw2" /*onChange={e=>setPw2(e.target.value)}*/onChange={change}/>
+					<input type="password" className="form-control" id="pw2" placeholder="비밀번호를 입력하세요." name="me_pw2" value={data.me_pw2} /*onChange={e=>setPw2(e.target.value)}*/onChange={change}/>
 				</div>
 				<div className="mb-3 mt-3">
 					<label htmlFor="email" className="form-label">이메일:</label>
-					<input type="email" className="form-control" id="email" placeholder="이메일주소를 입력하세요." name="me_email" /*onChange={e=>setEmail(e.target.value)}*/onChange={change}/>
+					<input type="email" className="form-control" id="email" placeholder="이메일주소를 입력하세요." name="me_email" value={data.me_email} /*onChange={e=>setEmail(e.target.value)}*/onChange={change}/>
 				</div>
 				<button className="btn btn-outline-success col-12">회원가입</button>
 			</form>
