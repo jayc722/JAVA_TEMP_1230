1c91dfeb5ba7ae063e4a62792600a8321e045b4e
jayc722 <jaewon8469@gmail.com>
2025-04-10 16:07:10 +0900

    강사님(e.target으로 여러 정보 한꺼번에 가져오기)

--- react/react1/src/App2.js
+++ react/react1/src/App2.js
@@ -7,25 +7,32 @@ import { useState } from "react";
 
 
 function App2(){
-	let [state,setState] = useState("");
-	function checked(){
-		
+	let [hobby,setHobby] = useState("");
+	function change(e){
+		//let target = e.target;
+		//console.log(target);
+
+		let {value, name, checked} = e.target;
+		//console.log(value);
+		//console.log(name);
+		//console.log(checked);
+		setHobby(value);
 	}
 	return(
 		<div>
 			<label>
-				<input type="radio" value={"독서"} name="hobby" onClick={(e)=>{setState(e.currentTarget.value)}}/> 독서
+				<input type="radio" value={"독서"} name="hobby" onClick={change}/> 독서
 			</label>
 			<label>
-				<input type="radio" value={"운동"} name="hobby" onClick={(e)=>{setState(e.currentTarget.value)}}/> 운동
+				<input type="radio" value={"운동"} name="hobby" onClick={change}/> 운동
 			</label>
 			<label>
-				<input type="radio" value={"음악감상"} name="hobby" onClick={(e)=>{setState(e.currentTarget.value)}}/> 음악감상
+				<input type="radio" value={"음악감상"} name="hobby" onClick={change}/> 음악감상
 			</label>
 			<label>
-				<input type="radio" value={""} name="hobby" onClick={(e)=>{setState(e.currentTarget.value)}}/> 해당없음
+				<input type="radio" value={""} name="hobby" onClick={change}/> 해당없음
 			</label>
-			<h1>{state}</h1>
+			<h1>{hobby}</h1>
 
 		</div>
 	)
