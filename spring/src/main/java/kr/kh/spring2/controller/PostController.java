2de1094ede7bf5a064a54ab470b7f0ffd3e4a30b
jayc722 <jaewon8469@gmail.com>
2025-04-14 09:48:50 +0900

    autowired 해서 이상 없는지 확인

--- spring/src/main/java/kr/kh/spring2/controller/PostController.java
+++ spring/src/main/java/kr/kh/spring2/controller/PostController.java
@@ -1,13 +1,18 @@
 package kr.kh.spring2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.kh.spring2.service.PostService;
 
 @Controller
 @RequestMapping("/post")
 public class PostController {
 
+	@Autowired
+	private PostService postService;
 	
 	@GetMapping("/list")
 	public String list() {
--- spring/src/main/java/kr/kh/spring2/service/PostServiceImp.java
+++ spring/src/main/java/kr/kh/spring2/service/PostServiceImp.java
@@ -1,10 +1,15 @@
 package kr.kh.spring2.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.kh.spring2.dao.PostDao;
 
 @Service
 public class PostServiceImp implements PostService{
 
+	@Autowired
+	private PostDao postDao;
 	
 	
 }
