119defba7205c667d636743a28c37b25fc5d3e7a
jayc722 <jaewon8469@gmail.com>
2025-04-21 15:21:49 +0900

    게시판에 맞는 게시글 가져와서 콘솔창에 띄움

--- boot/boot/src/main/java/kr/kh/boot/controller/PostController.java
+++ boot/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -1,11 +1,14 @@
 package kr.kh.boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.kh.boot.model.vo.PostVO;
 import kr.kh.boot.service.PostService;
 
 
@@ -17,7 +20,9 @@ public class PostController {
 
 	@GetMapping("/post/list/{bo_num}")
 	public String postList(@PathVariable int bo_num){
-		System.out.println(bo_num);
+		//System.out.println(bo_num);
+		List<PostVO> list = postService.getPostList(bo_num);
+		System.out.println(list);
 		return "post/list";
 	}
 
