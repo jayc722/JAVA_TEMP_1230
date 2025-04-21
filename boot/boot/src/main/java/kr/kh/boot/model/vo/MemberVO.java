42e2d3978c04bcb73c4f9e456b394d0b296b0225
jayc722 <jaewon8469@gmail.com>
2025-04-21 11:24:31 +0900

    vo생성, lombok 의존성 추가

---
+++ boot/boot/src/main/java/kr/kh/boot/model/vo/MemberVO.java
@@ -0,0 +1,16 @@
+package kr.kh.boot.model.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+
+	String me_id;
+	String me_pw;
+	String me_email;
+	String me_iauthority;
+	String me_cookie;
+	String me_limit;
+
+	
+}
