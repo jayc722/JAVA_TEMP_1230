805529d3e36a569048f947e5612792faefd82474
jayc722 <jaewon8469@gmail.com>
2025-04-21 10:40:46 +0900

    스프링부트 사용

--- boot/boot/src/main/java/kr/kh/boot/controller/HomeController.java
+++ boot/boot/src/main/java/kr/kh/boot/controller/HomeController.java
@@ -3,6 +3,7 @@ package kr.kh.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -16,8 +17,29 @@ public class HomeController {
 		return "index";
 	}
 	@GetMapping("/test")
-	public String test() {
+	public String test(Model model) {
+		int num = (int)(Math.random()*10);		//num에 0에서 9 사이 값 랜덤으로 넣기
+		String role = "";
+		switch (num) {
+			case 4,6,8:
+				role = "ADMIN";
+				break;
+		
+			case 3,5,7,9:
+				role = "USER";
+				break;
+		}//짝수는 관리자 홀수는 유저, 012는 게스트
+
+		model.addAttribute("num", num);
+		model.addAttribute("role", role);
 		return "test";
 	}
+	@GetMapping("/test/{num1}")			//pathvariable에 없어서 에러 뜸
+	//@GetMapping("/test/{num}")
+	public String testNum(@PathVariable("num1") int num) {	//pathvariable에 들어갈 변수명 // 중괄호 안에서 사용할 변수명
+		System.out.println(num);
+		return "test";	//화면은 test.html 그대로 활용
+	}
+	
 	
 }
