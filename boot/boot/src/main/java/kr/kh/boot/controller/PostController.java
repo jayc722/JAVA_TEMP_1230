fd2f6ae249a4e903109aab9f7ce67c1c13bc895e
jayc722 <jaewon8469@gmail.com>
2025-04-21 15:42:53 +0900

    게시판 예제-내가한거

--- boot/boot/src/main/java/kr/kh/boot/controller/PostController.java
+++ boot/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,10 +20,11 @@ public class PostController {
 	PostService postService;
 
 	@GetMapping("/post/list/{bo_num}")
-	public String postList(@PathVariable int bo_num){
+	public String postList(Model model, @PathVariable int bo_num){
 		//System.out.println(bo_num);
 		List<PostVO> list = postService.getPostList(bo_num);
-		System.out.println(list);
+		//System.out.println(list);
+		model.addAttribute("postList", list);
 		return "post/list";
 	}
 
