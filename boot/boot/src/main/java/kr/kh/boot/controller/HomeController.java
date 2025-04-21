2620e64e6324595883ce5cc5c47ece82bb12501c
jayc722 <jaewon8469@gmail.com>
2025-04-21 10:13:36 +0900

    타임리프1

--- boot/boot/src/main/java/kr/kh/boot/controller/HomeController.java
+++ boot/boot/src/main/java/kr/kh/boot/controller/HomeController.java
@@ -3,6 +3,8 @@ package kr.kh.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -13,4 +15,9 @@ public class HomeController {
 		model.addAttribute("name", "홍길동");
 		return "index";
 	}
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+	
 }
