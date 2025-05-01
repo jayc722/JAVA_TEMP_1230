4e9544db5ead9f8f151e6c1ad0f160e8fb78ccec
jayc722 <jaewon8469@gmail.com>
2025-05-01 12:26:19 +0900

    디테일창 기본구조 잡음

--- spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/ProductController.java
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/ProductController.java
@@ -28,4 +28,11 @@ public class ProductController {
 		return "product/list";
 	}
 	
+
+	
+	@GetMapping("/detail")
+	public String detail(Model model, String pr_code) {
+
+		return "product/detail";
+	}
 }
